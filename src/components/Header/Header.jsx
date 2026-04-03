410be04760113cf7fd59a3d439ac60c91ffe24bd
IagoGoulart <iagogoulart15@gmail.com>
2026-04-03 16:44:11 -0300

    Corrige links das publicações no header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import * as S from "./styles"
+import * as S from "./styles";
 import { Link } from "react-router-dom";
 import coroa from "../../assets/coroa.png";
 
 export default function Header() {
-  return(
+  return (
     <S.Container>
       <S.Header>
         <S.LogoArea>
@@ -16,75 +16,151 @@ export default function Header() {
 
         <S.Nav>
           <ul>
-            <li><Link to="/">Início</Link></li>
+            <li>
+              <Link to="/">Início</Link>
+            </li>
             <li>
               Sobre
               <ul>
-                <li><Link to="/sobre/quem-somos">Quem somos</Link></li>
-                <li><Link to="/sobre/simbolos-nacionais">Símbolos nacionais</Link></li>
-                <li><Link to="/sobre/noticias">Notícias</Link></li>
+                <li>
+                  <Link to="/sobre/quem-somos">Quem somos</Link>
+                </li>
+                <li>
+                  <Link to="/sobre/simbolos-nacionais">Símbolos nacionais</Link>
+                </li>
+                <li>
+                  <Link to="/sobre/noticias">Notícias</Link>
+                </li>
               </ul>
             </li>
 
             <li>
               Coroa
               <ul>
-                <li><Link to="/coroa/coroa-pantaneira">A Coroa Pantaneira</Link></li>
-                <li><Link to="/coroa/sua-alteza-real">Sua Alteza Real</Link></li>
-                <li><Link to="/coroa/casa-grao-ducal">A Casa Grão-Ducal</Link></li>
-                <li><Link to="/coroa/estatuto-dinastico">Estatuto Dinástico</Link></li>
-                <li><Link to="/coroa/nobreza-aristocracia">Nobreza e Aristocracia</Link></li>
-                <li><Link to="/coroa/codigo-nobiliarchico">Código Nobiliárquico</Link></li>
-                <li><Link to="/coroa/ordens-condecoracoes">Ordens e Condecorações</Link></li>
+                <li>
+                  <Link to="/coroa/coroa-pantaneira">A Coroa Pantaneira</Link>
+                </li>
+                <li>
+                  <Link to="/coroa/sua-alteza-real">Sua Alteza Real</Link>
+                </li>
+                <li>
+                  <Link to="/coroa/casa-grao-ducal">A Casa Grão-Ducal</Link>
+                </li>
+                <li>
+                  <Link to="/coroa/estatuto-dinastico">Estatuto Dinástico</Link>
+                </li>
+                <li>
+                  <Link to="/coroa/nobreza-aristocracia">
+                    Nobreza e Aristocracia
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/coroa/codigo-nobiliarchico">
+                    Código Nobiliárquico
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/coroa/ordens-condecoracoes">
+                    Ordens e Condecorações
+                  </Link>
+                </li>
               </ul>
             </li>
 
             <li>
               Governo
               <ul>
-                <li><Link to="/governo/conselho-governo">Conselho de Governo</Link></li>
-                <li><Link to="/governo/chanceler">O Chanceler</Link></li>
-                <li><Link to="/governo/banco-central">Banco Central</Link></li>
+                <li>
+                  <Link to="/governo/conselho-governo">
+                    Conselho de Governo
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/governo/chanceler">O Chanceler</Link>
+                </li>
+                <li>
+                  <Link to="/governo/banco-central">Banco Central</Link>
+                </li>
               </ul>
             </li>
 
             <li>
               Legislativo
               <ul>
-                <li><Link to="/legislativo/parlamento">O Parlamento</Link></li>
-                <li><Link to="/legislativo/legislacao">Legislação</Link></li>
+                <li>
+                  <Link to="/legislativo/parlamento">O Parlamento</Link>
+                </li>
+                <li>
+                  <Link to="/legislativo/legislacao">Legislação</Link>
+                </li>
               </ul>
             </li>
 
             <li>
               Judiciário
               <ul>
-                <li><Link to="/judiciario/supremo-tribunal">Supremo Tribunal</Link></li>
-                <li><Link to="/judiciario/diario-justica">Diário de Justiça</Link></li>
+                <li>
+                  <Link to="/judiciario/supremo-tribunal">
+                    Supremo Tribunal
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/judiciario/diario-justica">Diário de Justiça</Link>
+                </li>
               </ul>
             </li>
 
             <li>
               Diplomacia
               <ul>
-                <li><Link to="/diplomacia/embaixadas-consulados">Embaixadas e consulados</Link></li>
-                <li><Link to="/diplomacia/tratados-internacionais">Tratados internacionais</Link></li>
+                <li>
+                  <Link to="/diplomacia/embaixadas-consulados">
+                    Embaixadas e consulados
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/diplomacia/tratados-internacionais">
+                    Tratados internacionais
+                  </Link>
+                </li>
               </ul>
             </li>
 
             <li>
               Arquivo
               <ul>
-                <li><Link to="/arquivo/constituicao-nacional">Constituição Nacional</Link></li>
-                <li className="has-submenu">
-                  <span>Publicações ›</span>
+                <li>
+                  <Link to="/arquivo/constituicao-nacional">
+                    Constituição Nacional
+                  </Link>
+                </li>
+                <li>
+                  Publicações
                   <ul className="submenu">
-                    <li><Link to="/arquivo/publicacoes/decreto-grao-ducal">Decreto Grão-Ducal</Link></li>
-                    <li><Link to="/arquivo/publicacoes/ato-da-coroa">Ato da Coroa</Link></li>
-                    <li><Link to="/arquivo/publicacoes/ordem-executiva">Ordem Executiva</Link></li>
-                    <li><Link to="/arquivo/publicacoes/portaria">Portaria</Link></li>
-                    <li><Link to="/arquivo/publicacoes/medida-provisoria">Medida Provisória</Link></li>
-                    <li><Link to="/arquivo/publicacoes/tratados">Tratados</Link></li>
+                    <li>
+                      <Link to="/arquivo/publicacoes/decreto">
+                        Decreto Grão-Ducal
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/arquivo/publicacoes/ato">Ato da Coroa</Link>
+                    </li>
+                    <li>
+                      <Link to="/arquivo/publicacoes/ordem">
+                        Ordem Executiva
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/arquivo/publicacoes/portaria">Portaria</Link>
+                    </li>
+                    <li>
+                      <Link to="/arquivo/publicacoes/medida">
+                        Medida Provisória
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/arquivo/publicacoes/tratado">Tratado</Link>
+                    </li>
                   </ul>
                 </li>
               </ul>
